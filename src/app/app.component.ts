caba1146929ee16357c319a38f390da54c879761
rgunatilake <rgunatilake@objectconsulting.com.au>
2017-11-12 19:51:42 +1100

    initial-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BusDataService} from './services/bus-data.service';
+import {BusOrg} from "./model/bus-organization";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private busOrg: BusOrg;
+  isGroupOpen = false;
+  note: string;
+  notes: string[] = [];
+
+  constructor(private dataService: BusDataService) {
+    dataService.getBusData().subscribe(
+      data => {
+        this.busOrg = data;
+       }
+      , error => console.log(error));
+  }
+
+  getFirstRoutePart(routeNum: string): string {
+    if (routeNum.toLowerCase() === 'unknown') {
+      return '';
+    }
+    return routeNum.slice(0, 3);
+  }
+
+  getSecondRoutePart(routeNum: string): string {
+    if (routeNum.toLowerCase() === 'unknown') {
+      return 'Unknown';
+    }
+    return routeNum.slice(3, routeNum.length);
+  }
+
+  toggleRoutePanel(): void {
+    this.isGroupOpen = (this.isGroupOpen) ? false : true;
+  }
+
+  submitForm(idx: string) {
+    ;
+    console.log("Notes being submitted for '" + this.busOrg.data[idx].organisation + "' are : " + this.notes[idx]);
+    //This needs to be posted to server
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BusDataService} from './services/bus-data.service';
+import {AccordionModule, Ng2BootstrapModule} from 'ng2-bootstrap';
+import {TnAccordionComponent} from './shared-components/tn-accordion/tn-accordion.component';
+import {BusStatusPipe} from './pipes/bus-status.pipe';
+import {BusStatusColorPipe} from './pipes/bus-status-color.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TnAccordionComponent,
+    BusStatusPipe,
+    BusStatusColorPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2BootstrapModule.forRoot(),
   ],
-  providers: [],
+  exports: [
+    Ng2BootstrapModule,
+    TnAccordionComponent,
+  ],
+  providers: [BusDataService, BusStatusPipe, BusStatusColorPipe],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
